7171fabb0cb01d110275e52b34215dd065d8b954
obinmarsus25-create <obinmarsus25@gmail.com>
2026-04-22 06:51:58 +0100

    first upload

---
+++ projek1.py
@@ -0,0 +1,17 @@
+nama = input("masukan nama anda:")
+
+if nama =("obin marsus")
+    prin("selamat datang obin marsus")
+     
+umur = input("masukan umur anda")
+
+if umur==20:
+    print("anda cukup umur:")
+elif umur==0:
+    print("anda belum lahir")
+elif umur==17:
+    prin("anda belum cukup umur")
+elif umur ==105:
+    print("islam itu indah") 
+
+print("program selsai")
