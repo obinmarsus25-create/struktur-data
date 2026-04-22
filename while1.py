e0b1ae7bfec64726054442e932af757f8ff24e10
obinmarsus25-create <obinmarsus25@gmail.com>
2026-04-22 06:50:48 +0100

    first upload

---
+++ while1.py
@@ -0,0 +1,13 @@
+# while loop
+
+# kondisi
+# aksi
+#  
+angka = 0
+print(f"angka sekrang ={angka}")
+
+while angka < 4 :
+    angka += 1
+    print(f"angka seekrang = {angka}")
+    print("obin anak undikma")
+print("program selesai")
